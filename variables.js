0ba87edcf844f90fedb236b6239c85c01ab4ca93
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-09-24 09:22:56 -0600

    Variables Problem 3

--- variables.js
+++ variables.js
@@ -14,6 +14,7 @@ let faveNum = 311;
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 /*
