8f7de2c09f9f9a31845445f5febc071b2cb995ca
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-09-24 09:20:15 -0600

    Variables Problem 1

--- variables.js
+++ variables.js
@@ -2,6 +2,7 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+let myName = "Dillon";
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
